acb5944f87bc2954c3d1eaade7b89f2205866afe
k0 <k0src@null.net>
2025-09-20 02:08:56 -0500

    fix line wrapping

--- src/TypstEditor.ts
+++ src/TypstEditor.ts
@@ -55,6 +55,8 @@ export class TypstEditor {
       highlightActiveLine(),
       history(),
 
+      EditorView.lineWrapping,
+
       // Language features
       bracketMatching(),
       closeBrackets(),
